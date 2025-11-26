523c1c0b1bf6d3d6674a15b992321166940c50bc
Cillian <cillian.murchu@gmail.com>
2025-11-26 23:44:53 +0100

    fix build

--- src/utils/skillsButtonPosition.ts
+++
@@ -1,7 +0,0 @@
-// Global variable to store the Skills button position for icon animations
-export let skillsButtonPosition: { x: number; y: number } | null = null;
-
-// Function to update the button position
-export const setSkillsButtonPosition = (position: { x: number; y: number } | null) => {
-  skillsButtonPosition = position;
-};
