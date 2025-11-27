9321bbfcd0856a857fc7454c11239d0484510310
Cillian <cillian.murchu@gmail.com>
2025-11-27 19:46:04 +0100

    fix footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import "./site-footer.css";
 
 const Footer: React.FC = () => (
   <footer
-    className="site-footer w-full bg-gray-900 text-gray-300 py-6 flex flex-col items-center 
+    className="site-footer z-100 w-full bg-gray-900 text-gray-300 py-6 flex flex-col items-center 
   justify-center relative border-top-neon"
   >
     <div className="absolute top-0 left-0 w-full" />
